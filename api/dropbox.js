8e5d5d81457c3b2f8dd0005d66acf13d4e4daf5e
thqrs <143814963+thqrs@users.noreply.github.com>
2023-09-26 17:27:30 +0100

    Update dropbox.js

--- api/dropbox.js
+++ api/dropbox.js
@@ -1,28 +1,33 @@
 const Dropbox = require('dropbox').Dropbox;
 const fetch = require('node-fetch');
 
-// Define the path to your Excel file in Dropbox
 const filePath = "/YourDropboxFolderPath/YourExcelFileName.xlsx";
+
 module.exports = async (req, res) => {
     try {
+        console.log('Function Invoked');
         console.log('Request Body:', req.body);
-    const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN, fetch: fetch });
-    const { action, coordinates, text } = req.body;
+        
+        const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN, fetch: fetch });
+        console.log('Dropbox Instance Created');
+        
+        const { action, coordinates, text } = req.body;
+        console.log('Action:', action);
 
-    if (action === 'read') {
-        // Use filePath when reading the Excel file from Dropbox
-        // Implement logic to read from the Excel file at the specified filePath
-        res.status(200).send({ success: true, message: "Read action received!" });
-    } else if (action === 'write') {
-        // Use filePath when writing to the Excel file in Dropbox
-        // Implement logic to write to the Excel file at the specified filePath
-        res.status(200).send({ success: true, message: "Write action received!" });
-    } else {
-        res.status(400).send({ success: false, message: "Invalid action!" });
-    }
-            } catch (error) {
+        if (action === 'read') {
+            console.log('Reading from Excel');
+            // Implement logic to read from the Excel file
+            res.status(200).send({ success: true, message: "Read action received!" });
+        } else if (action === 'write') {
+            console.log('Writing to Excel');
+            // Implement logic to write to the Excel file
+            res.status(200).send({ success: true, message: "Write action received!" });
+        } else {
+            console.log('Invalid Action');
+            res.status(400).send({ success: false, message: "Invalid action!" });
+        }
+    } catch (error) {
         console.error('Error:', error.message);
         res.status(500).send({ error: error.message });
     }
 };
-
