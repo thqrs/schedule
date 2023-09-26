a07aceba344b6e2a8156a66203e3622675bd1940
thqrs <143814963+thqrs@users.noreply.github.com>
2023-09-26 17:20:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,24 +13,19 @@ async function writeText() {
 }
 
 async function readText() {
-    // Get the coordinates from the input box
     const coordinates = document.getElementById('readCoordinates').value;
 
-    // Call your serverless function (Vercel function) to read text from Excel
     const response = await fetch('/api/dropbox', {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ action: 'read', coordinates: coordinates }),
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ action: 'read', coordinates })
     });
 
-    // Parse the response from the serverless function
     const result = await response.json();
-
-    // Update the "output" text box with the loaded text
     document.getElementById('output').value = result.text;
 }
 
-// Attach the readText function to the corresponding button
-document.getElementById('readButton').addEventListener('click', readText);
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('readButton').addEventListener('click', readText);
+    document.getElementById('writeButton').addEventListener('click', writeText);
+});
