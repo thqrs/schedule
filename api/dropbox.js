b9a315a46858916312ef926ab09c357aa22c82f2
thqrs <143814963+thqrs@users.noreply.github.com>
2023-09-26 15:34:54 +0100

    Update dropbox.js

--- api/dropbox.js
+++ api/dropbox.js
@@ -1,26 +1,22 @@
-// Import necessary modules
 const Dropbox = require('dropbox').Dropbox;
 const fetch = require('node-fetch');
 
-// Define the serverless function
-module.exports = async (req, res) => {
-    // Initialize Dropbox with the access token from environment variables
-    const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN, fetch: fetch });
+// Define the path to your Excel file in Dropbox
+const filePath = "/YourDropboxFolderPath/YourExcelFileName.xlsx";
 
-    // Extract action, coordinates, and text from the request body
+module.exports = async (req, res) => {
+    const dbx = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN, fetch: fetch });
     const { action, coordinates, text } = req.body;
 
-    // Check the action type
     if (action === 'read') {
-        // If action is 'read', implement logic to read from the Excel file in Dropbox
-        // For now, let's just send a placeholder message
+        // Use filePath when reading the Excel file from Dropbox
+        // Implement logic to read from the Excel file at the specified filePath
         res.status(200).send({ success: true, message: "Read action received!" });
     } else if (action === 'write') {
-        // If action is 'write', implement logic to write to the Excel file in Dropbox
-        // For now, let's just send a placeholder message
+        // Use filePath when writing to the Excel file in Dropbox
+        // Implement logic to write to the Excel file at the specified filePath
         res.status(200).send({ success: true, message: "Write action received!" });
     } else {
-        // If action is neither 'read' nor 'write', send an error message
         res.status(400).send({ success: false, message: "Invalid action!" });
     }
 };
