c62f948271404559643cf1d012300c3465f4c7a3
thqrs <143814963+thqrs@users.noreply.github.com>
2023-09-26 14:02:19 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+async function writeText() {
+    const coordinates = document.getElementById('coordinates').value;
+    const text = document.getElementById('text').value;
+
+    const response = await fetch('/api/dropbox', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ action: 'write', coordinates, text })
+    });
+
+    const result = await response.json();
+    // Handle the response
+}
+
+async function readText() {
+    const coordinates = document.getElementById('readCoordinates').value;
+
+    const response = await fetch('/api/dropbox', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ action: 'read', coordinates })
+    });
+
+    const result = await response.json();
+    document.getElementById('output').textContent = result.text;
+}
